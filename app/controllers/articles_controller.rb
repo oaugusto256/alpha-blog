cbcb90e4310a439df4b79f32fd19d275a3215277
Otavio Augusto <tavioalves@gmail.com>
2017-06-14 00:46:05 -0300

    Versão atual do projeto

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,54 @@
+class ArticlesController < ApplicationController
+	before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+	def index
+		@articles = Article.paginate(page: params[:page], per_page: 3)
+	end
+
+	def new
+		@article = Article.new	
+	end
+
+	def create
+		
+		@article = Article.new(article_params)
+		@article.user = User.first
+		if @article.save
+			flash[:success] = "Article was successfully created"
+			redirect_to article_path(@article)
+		else
+			render :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @article.update(article_params)
+			flash[:success] = "Article was successfully updated"
+			redirect_to article_path(@article)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@article.destroy
+		flash[:danger] = "Article was successfully deleted"
+		redirect_to articles_path
+	end
+
+	def show
+	end	
+
+	private
+		def set_article
+			@article = Article.find(params[:id])
+		end
+
+		def article_params
+			params.require(:article).permit(:title, :description)
+		end
+
+end
